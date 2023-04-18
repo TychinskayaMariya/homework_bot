a7062d15fc743ed851d04cf33cb0a4e4d02a9649
Мария Тычинская <tychinskayamariya@gmail.com>
2023-04-18 12:04:43 +0300

    Correct mistakes after first review

--- homework.py
+++ homework.py
@@ -21,6 +21,10 @@ TELEGRAM_CHAT_ID = os.getenv('TELEGRAM_CHAT_ID')
 RETRY_PERIOD = 600
 ENDPOINT = 'https://practicum.yandex.ru/api/user_api/homework_statuses/'
 HEADERS = {'Authorization': f'OAuth {PRACTICUM_TOKEN}'}
+ERROR_SEND_MESSAGE = 'Отсутствует доступ к серверу Telegram'
+ERROR_GET_API_ANSWER = 'Отсутствует доступ к серверу Яндекс.Практикум'
+ERROR_CHECK_RESPONSE = 'Некорректный ответ API'
+ERROR_PARSE_STATUS = 'Недокументированный статус проверки или отсуствие ключа'
 
 
 HOMEWORK_VERDICTS = {
@@ -34,28 +38,27 @@ logger = logging.getLogger(__name__)
 
 def check_tokens() -> bool:
     """Проверяет доступность переменных окружения."""
-    list_env = [
+    list_tokens = [
         PRACTICUM_TOKEN,
         TELEGRAM_TOKEN,
         TELEGRAM_CHAT_ID
     ]
-    return all(list_env)
+    return all(list_tokens)
 
 
 def send_message(bot: telegram.Bot, message: str) -> None:
     """Отправляет сообщение в Telegram-чат."""
-    error_text = 'Отсутствует доступ к серверу Telegram'
     try:
         bot.send_message(TELEGRAM_CHAT_ID, message)
         logger.debug('Успешная отправка сообщения в Telegram')
     except Exception:
-        logger.error(error_text, exc_info=True)
+        logger.error(ERROR_SEND_MESSAGE, exc_info=True)
 
 
 def get_api_answer(timestamp: int) -> Union[dict, str]:
     """Делает запрос к эндпоинту API-сервиса."""
     payload = {'from_date': timestamp}
-    error_text = 'Отсутствует доступ к серверу Яндекс.Практикум'
+    logger.info('Начало запроса к серверу Яндекс.Практикум')
     try:
         homework_statuses = requests.get(
             ENDPOINT,
@@ -63,35 +66,33 @@ def get_api_answer(timestamp: int) -> Union[dict, str]:
             params=payload
         )
         if homework_statuses.status_code != HTTPStatus.OK:
-            raise HTTPRequestError(error_text)
+            raise HTTPRequestError(ERROR_GET_API_ANSWER)
         return homework_statuses.json()
     except Exception:
-        raise HTTPRequestError(error_text)
+        raise HTTPRequestError(ERROR_GET_API_ANSWER)
 
 
 def check_response(response: dict) -> list:
     """Проверяет ответ API на соответствие документации."""
-    error_text = 'Некорректный ответ API'
     if not isinstance(response, dict):
-        raise TypeError(error_text)
+        raise TypeError(ERROR_CHECK_RESPONSE)
     if 'homeworks' not in response:
-        raise KeyError(error_text)
+        raise KeyError(ERROR_CHECK_RESPONSE)
     if not isinstance(response.get('homeworks'), list):
-        raise TypeError(error_text)
+        raise TypeError(ERROR_CHECK_RESPONSE)
     return response['homeworks']
 
 
 def parse_status(homework: dict) -> str:
     """Извлекает статус домашней работы."""
-    error_text = 'Недокументированный статус проверки или отсуствие ключа'
     if 'homework_name' not in homework or 'status' not in homework:
-        raise KeyError(error_text)
+        raise KeyError(ERROR_PARSE_STATUS)
     homework_name = homework['homework_name']
     status = homework['status']
     if not isinstance(homework_name, str) or not isinstance(status, str):
-        raise TypeError(error_text)
+        raise TypeError(ERROR_PARSE_STATUS)
     if status not in HOMEWORK_VERDICTS:
-        raise KeyError(error_text)
+        raise KeyError(ERROR_PARSE_STATUS)
     verdict = HOMEWORK_VERDICTS[status]
     return f'Изменился статус проверки работы "{homework_name}". {verdict}'
 
@@ -112,12 +113,12 @@ def main():
     while True:
         try:
             response = get_api_answer(timestamp)
-            correct_response = check_response(response)
-            if len(correct_response) == 0:
+            homework = check_response(response)
+            if not homework:
                 message = 'Ответ API пуст: нет домашних работ.'
                 logger.debug(message)
             else:
-                message = parse_status(correct_response[0])
+                message = parse_status(homework[0])
             if old_message != message:
                 old_message = message
                 send_message(bot, message)
